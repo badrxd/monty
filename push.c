9c84bd16964734448b2b2b9bf26a6d573edc1315
badrxd <badrxdxd@gmail.com>
2023-06-22 07:39:07 +0100

    ok

--- push.c
+++ push.c
@@ -36,24 +36,3 @@ void f_push(stack_t **head, unsigned int counter)
 		add_queue(head, n);
 }
 
-
-/**
- * f_pall - prints the stack (doubly list)
- * @head: stack head
- * @counter: will not use it
- * Return: no return
-*/
-void f_pall(stack_t **head, unsigned int counter)
-{
-	stack_t *temp;
-	(void)counter;
-
-	temp = *head;
-	if (temp == NULL)
-		return;
-	while (temp)
-	{
-		printf("%d\n", temp->n);
-		temp = temp->next;
-	}
-}
