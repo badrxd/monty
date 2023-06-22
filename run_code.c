8813eef34d00d54af02aea0b632873f489779725
assouljawad <38533122+assouljawad@users.noreply.github.com>
2023-06-22 21:44:08 +0100

    monty

--- run_code.c
+++ run_code.c
@@ -12,7 +12,7 @@ int run_code(char *content, stack_t **head, unsigned int counter)
 				{"push", f_push}, {"pall", f_pall}, {"pint", pint_f},
 				{"pop", pop_f}, {"swap", swap_f}, {"add", add_f},
 				{"nop", nop_f}, {"sub", sub_f}, {"div", div_f},
-				{"mul", mul_f}				
+				{"mul", mul_f},
 				{NULL, NULL}
 				};
 	unsigned int i = 0;
